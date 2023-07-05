f6cd93fd64b4ee7ba00fa09d323099128c7aa9d4
yashb <yashbhotmagey.resoluteai@gmail.com>
2023-07-05 11:31:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from st_custom_components import st_audiorec
 import os
+import wave
 # import speech_recognition as sr
 
 folder_path = 'audio_files'  
@@ -15,10 +16,15 @@ def audiorec_demo_app():
         # display audio data as received on the Python side
         col_playback, col_space = st.columns([0.58,0.42])
         with col_playback:
-            st.audio(wav_audio_data, format='audio/wav')
+            # st.audio(wav_audio_data, format='audio/wav')
             file_path = os.path.join(folder_path, '1.wav')
             with open(file_path, 'wb') as file:
                 file.write(wav_audio_data)
             print(f"Audio file saved at: {file_path}")
-            
+
 audiorec_demo_app()
+
+saved_file = wave.open(os.path.join(folder_path, '1.wav'))
+
+if saved_file is not None:
+    st.audio(saved_file, format='audio/wav')
